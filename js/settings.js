72b79de60b0eb8436035d4d207c4659fefe9c6f8
Svaltqt <naxxudea@gmail.com>
2023-06-12 19:36:50 -0500

    update

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,32 @@
 $(document).ready(function()
         { 
+
             
             $("#salir").click(function()
             {  window.location.href = "./index.html";
             });
-        });
+
+            var modal = $("#modalCuenta");
+            var OpenModal = $("#abrirCuenta");            
+            var form = $("#cuenta-form");
+
+              OpenModal.click(function() {
+                modal.show();
+              });            
+              
+            
+              $(window).click(function(event) {
+                if (event.target == modal[0]) {
+                  modal.hide();
+                }
+              });
+            
+              form.submit(function(event) {
+                event.preventDefault(); // Evitar el envío del formulario
+                // Aquí puedes realizar las acciones necesarias al hacer clic en el botón "Guardar"
+                console.log("Formulario enviado");
+                modal.hide();
+              });
+});
+      
 
